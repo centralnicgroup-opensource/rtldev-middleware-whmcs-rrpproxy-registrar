fa182970b2d333cf35dd50a9de173521c8a3b30c
Kai Schwarz <kschwarz@hexonet.net>
2021-01-28 11:47:15 +0100

    fix(ci): migration to github actions and gulp (vs. Travis CI and make)

--- resources/domains/additionalfields.php
+++ resources/domains/additionalfields.php
@@ -1,4 +1,5 @@
 <?php
+
 $rrp_additionalfields = implode(DIRECTORY_SEPARATOR, array(ROOTDIR, "modules", "registrars", "keysystems", "lib", "additionalfields.php"));
 if (file_exists($rrp_additionalfields)) {
     include $rrp_additionalfields;
--- scripts/phpcompatibility.sh
+++ scripts/phpcompatibility.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PHP_VERSION="$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')"
-phpcs --standard=PHPCompatibility -q -n --colors --runtime-set testVersion "$PHP_VERSION" modules || exit 1;
+phpcs --standard=PHPCompatibility -q -n --colors --runtime-set testVersion "$PHP_VERSION" --extensions=php --ignore=node_modules,vendor,templates_c,.github,scripts . || exit 1;

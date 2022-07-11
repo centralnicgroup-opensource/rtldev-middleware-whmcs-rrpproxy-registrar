dd6ffd616e4108c79a868d1399269d8d194e7fbd
Asif Nawaz <asif.nawaz@centralnic.com>
2022-07-11 09:29:20 +0100

    chore(gulp): cleanup package-lock

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,60 @@
-const { series, src, dest } = require('gulp')
-const clean = require('gulp-clean')
-const zip = require('gulp-zip')
-const exec = require('util').promisify(require('child_process').exec)
-const cfg = require('./gulpfile.json')
+const { series, src, dest } = require("gulp");
+const clean = require("gulp-clean");
+const zip = require("gulp-zip");
+const exec = require("util").promisify(require("child_process").exec);
+const cfg = require("./gulpfile.json");
 
 /**
  * Perform PHP Linting
  */
-async function doLint()
-{
+async function doLint() {
   // these may fail, it's fine
-    try {
-        await exec(`${cfg.vendorbin}${cfg.phpcsfixcmd} ${cfg.phpcsparams}`)
-    } catch (e) {
-    }
+  try {
+    await exec(`${cfg.vendorbin}${cfg.phpcsfixcmd} ${cfg.phpcsparams}`);
+  } catch (e) {}
 
   // these shouldn't fail
-    try {
-        await exec(`${cfg.vendorbin}${cfg.phpcschkcmd} ${cfg.phpcsparams}`)
-        await exec(`${cfg.vendorbin}${cfg.phpcomptcmd} ${cfg.phpcsparams}`)
-        await exec(`${cfg.vendorbin}${cfg.phpstancmd}`);
-    } catch (e) {
-        await Promise.reject(e.message)
-    }
-    await Promise.resolve()
+  try {
+    await exec(`${cfg.vendorbin}${cfg.phpcschkcmd} ${cfg.phpcsparams}`);
+    await exec(`${cfg.vendorbin}${cfg.phpstancmd}`);
+  } catch (e) {
+    await Promise.reject(e.message);
+  }
+  await Promise.resolve();
 }
 
 /**
  * cleanup old build folder / archive
  * @return stream
  */
-function doDistClean()
-{
-    return src([cfg.archiveBuildPath, `${cfg.archiveFileName} - latest.zip`], { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+function doDistClean() {
+  return src([cfg.archiveBuildPath, `${cfg.archiveFileName} - latest.zip`], {
+    read: false,
+    base: ".",
+    allowEmpty: true,
+  }).pipe(clean({ force: true }));
 }
 
 /**
  * Copy all files/folders to build folder
  * @return stream
  */
-function doCopyFiles()
-{
-    return src(cfg.filesForArchive, { base: '.' })
-    .pipe(dest(cfg.archiveBuildPath))
+function doCopyFiles() {
+  return src(cfg.filesForArchive, { base: "." }).pipe(
+    dest(cfg.archiveBuildPath)
+  );
 }
 
 /**
  * Clean up files
  * @return stream
  */
-function doFullClean()
-{
-    return src(cfg.filesForCleanup, { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+function doFullClean() {
+  return src(cfg.filesForCleanup, {
+    read: false,
+    base: ".",
+    allowEmpty: true,
+  }).pipe(clean({ force: true }));
 }
 
 /**
@@ -63,7 +64,7 @@ function doFullClean()
 function doGitZip() {
   return src(`./${cfg.archiveBuildPath}/**`)
     .pipe(zip(`${cfg.archiveFileName}-latest.zip`))
-    .pipe(dest('.'))
+    .pipe(dest("."));
 }
 
 /**
@@ -73,35 +74,16 @@ function doGitZip() {
 function doZip() {
   return src(`./${cfg.archiveBuildPath}/**`)
     .pipe(zip(`${cfg.archiveFileName}.zip`))
-    .pipe(dest('./pkg'))
+    .pipe(dest("./pkg"));
 }
 
-exports.lint = series(
-  doLint
-)
+exports.lint = series(doLint);
 
-exports.copy = series(
-  doDistClean,
-  doCopyFiles
-)
+exports.copy = series(doDistClean, doCopyFiles);
 
-exports.prepare = series(
-  exports.lint,
-  exports.copy
-)
+exports.prepare = series(exports.lint, exports.copy);
 
-exports.archives = series(
-  doGitZip,
-  doZip
-)
+exports.archives = series(doGitZip, doZip);
 
-exports.default = series(
-  exports.prepare,
-  exports.archives,
-  doFullClean
-)
-exports.release = series(
-  exports.copy,
-  exports.archives,
-  doFullClean
-)
+exports.default = series(exports.prepare, exports.archives, doFullClean);
+exports.release = series(exports.copy, exports.archives, doFullClean);
